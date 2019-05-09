7a790967e01093a9323dcb2bdde1f7acac1c9b0f
alestjoh <ZUBEqevy42>
2019-05-09 15:26:56 -0400

    Create Twitter View Model

--- app/src/main/java/com/example/leastofficialpokedex/view/PokemonDetailsFragment.kt
+++ app/src/main/java/com/example/leastofficialpokedex/view/PokemonDetailsFragment.kt
@@ -25,16 +25,19 @@ class PokemonDetailsFragment(val pokemonName: String) : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val viewModel: PokemonViewModel = ViewModelProviders
+        val pokemonViewModel: PokemonViewModel = ViewModelProviders
             .of(this)
             .get(PokemonViewModel::class.java)
-        viewModel.pokemonData.observe(this, Observer {
+        pokemonViewModel.pokemonData.observe(this, Observer {
             Picasso.get().load(it.sprites.front_default).into(
                 iv_pokemon_detailsFragment,
                 ImageLoadCallback(progressBar_imageLoading_detailsFragment, this.context!!))
             iv_pokemon_detailsFragment.visibility = View.VISIBLE
         })
-        viewModel.getData(pokemonName)
+        pokemonViewModel.getData(pokemonName)
+
+
+
 
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_pokemon_details, container, false)
